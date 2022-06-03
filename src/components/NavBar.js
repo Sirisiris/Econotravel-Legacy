a59c2b8ee8b14c216dac6d61f56275775a72524e
eperez045 <eperez045@gmail.com>
2022-06-03 09:52:07 +0200

    navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,7 @@
 import { Link, Outlet } from "react-router-dom";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Logo from './images/logo.png';
 
 export default function NavBar() {
   return (
@@ -6,10 +9,16 @@ export default function NavBar() {
       <nav className="nav-bar">
         <ul>
           <li>
-            <Link to="/"> Home</Link>
+            <Link to="/"><img className="logo" src={Logo} alt="Logo" /> Home</Link>
           </li>
           <li>
             <Link to="search"> Search</Link>
+          </li>
+          <li>
+            <Link to="login"><AccountCircleIcon /> Login</Link>
+          </li>
+          <li>
+            <Link to="shopping"><ShoppingCartIcon /> Shopping</Link>
           </li>
         </ul>
       </nav>
