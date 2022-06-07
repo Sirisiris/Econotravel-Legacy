12e892ec003b7753623441051b43ec70cd4c4a5e
1ri5 <iriscyst@gmail.com>
2022-06-07 12:05:26 +0200

    #2 - JSON creado

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-import hero1 from '../images/hero-image1.png';
-import { Grid } from "@mui/material";
-
 export default class Main extends Component {
   render() {
     return (
-      <Grid container sm={12} xl={12} >
-       style={{backgroundImage: `url(${hero1})`}} 
-      </Grid>
+      <div>
+        <h1>Hola, Bienvenido al sistema</h1>
+      </div>
     );
   }
 }
