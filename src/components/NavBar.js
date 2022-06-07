8fb0bbe977c7890f54ce8df5ccaf2e55d5ec0279
Sara <sara13egea@hotmail.com>
2022-06-07 14:15:56 +0200

    Menú arreglado

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,29 +7,30 @@ import SearchIcon from '@mui/icons-material/Search';
 export default function NavBar() {
   return (
     <>
-    <Grid container>
-      <nav style={{display: "flex", alignItems: "center", justifyContent: "center", backgroundColor: '#fbf1e3'}}>
+    <Grid container style={{width: "100vw", backgroundColor: '#fbf1e3'}}>
+      
+      <nav style={{width:"100vw", display:"flex", flexDirection:"row", alignItems:"center", justifyContent:"space-around"}}>
          
-         <Grid item>
-            <Link to="/"><img src={Logo} style={{width: "30%"}} className="logo" alt="Logo"/></Link>
-          </Grid>
-          <Grid item sm={6}>
+            <Grid item lg={2}>
+            <Link to="/"><img src={Logo} style={{width: "40%", height:"40%"}} className="logo" alt="Logo"/></Link>
+            </Grid>
+            
             <Link to="search"></Link>
-            <Box style={{backgroundColor: '#b5bea7', display: 'flex', alignItems: 'center', borderRadius: '50px', width: '60%', minWidth: '15rem'}}>
-            <SearchIcon fontSize="large" style={{paddingLeft: '0.5rem'}}/>
+            <Box style={{ paddingLeft: "-4em", backgroundColor: '#b5bea7', display: 'flex', borderRadius: '50px', width: '60%', minWidth: '15rem'}}>
+            <SearchIcon style={{paddingLeft: '0.5rem'}}/>
             <InputBase
               placeholder="Search…"
               inputProps={{ 'aria-label': 'search' }}/>
               </Box>
-          </Grid>
-          <Grid item >
+          
+           <Grid item>
             <Link style={{color:"#2e5137"}} to="login"><AccountCircleIcon /> Login</Link>
         
         
             <Link  to="shopping" style={{color:"#2e5137"}}><ShoppingCartIcon /> Shopping</Link>
-          </Grid>
-        
+         </Grid>
       </nav>
+     
       </Grid>
       <Outlet />
     </>
