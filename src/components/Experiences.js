e59aa4fed791a88d42d629ab9ea4486146183465
Sara <sara13egea@hotmail.com>
2022-06-09 18:52:35 +0200

    Experiencias detalle añadido leerMás #3

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,47 +1,19 @@
 import * as React from 'react';
 import { Button,Container, Chip } from '@mui/material';
-import { Link } from "react-router-dom";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Logo from '../images/logo.png';
 import {Grid,Box, InputBase} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
 import exp1 from "../images/exp1.png";
+
+
+
 
 export default function Experiences () {
 
 
     return (
-
-        <>
-        <Grid container style= {{ backgroundColor: '#fbf1e3'}}>
-          
-          <nav style={{display:"flex", flexDirection:"row", alignItems:"center"}}>
-             
-                <Grid item lg={4}>
-                <Link to="/"><img src={Logo} style={{width: "30%", height:"30%", marginLeft:"6em"}} className="logo" alt="Logo"/></Link>
-                </Grid>
-                
-                <Grid item lg={6}>
-                <Link to="search"></Link>
-                <Box style={{paddingLeft: "4em", backgroundColor: '#b5bea7', display:"flex", borderRadius: '50px', minWidth: '15rem'}}>
-                <SearchIcon/>
-                <InputBase
-                  placeholder="Search…"
-                  inputProps={{'aria-label': 'search' }}/>
-                  </Box>
-              </Grid>
-               <Grid item lg={2} style={{display:"flex", paddingLeft:"10em"}}>
-                <Link style={{color:"#2e5137" }} to="login"><AccountCircleIcon /> Login</Link>
-            
-            
-                <Link  to="shopping" style={{color:"#2e5137", paddingLeft:"1em"}}><ShoppingCartIcon /> Shopping</Link>
-             </Grid>
-          </nav>
-         
-        
+  
+      <Grid container style= {{backgroundColor:"#FBF1E3"}}>
       
-      <Container style={{ marginTop:"6em"}}>
+      <Container style={{ marginTop:"6em" }}>
       
         <img src={exp1}
         width= "249"
@@ -64,12 +36,6 @@ export default function Experiences () {
 </Container>
 </Grid>
 
-     </>
-
-    
-      
-    
-      
       
       );
     }
