f0b54795c97d73201d4f40eb9cf950f34bf73d67
LAPBCN-013 <lauracalderininblue5@gmail.com>
2022-06-28 15:56:41 +0200

    cambio laura

--- client/src/components/ComoFunciona.js
+++ client/src/components/ComoFunciona.js
@@ -21,7 +21,7 @@ export default function ComoFunciona() {
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sagittis nec est sed posuere. Phasellus tempor a nibh ut scelerisque. Vestibulum in fringilla tellus, a finibus ligula. Maecenas dapibus tortor ut est mattis, scelerisque hendrerit lacus fringilla. Vivamus auctor interdum odio, in hendrerit enim dapibus ac. Mauris vehicula risus libero, rhoncus scelerisque lectus varius ac. Nulla sodales ipsum quam, non dictum neque condimentum sit amet. Vestibulum luctus scelerisque metus sit amet posuere.<br></br>
 
                     Praesent est nunc, consequat id turpis id, lobortis viverra dolor. Aenean non semper leo. Ut porta tincidunt mollis. Nam eleifend nunc sed consequat pellentesque. Aliquam ornare gravida enim, condimentum volutpat elit malesuada vitae. Ut finibus fringilla libero. Morbi felis odio, elementum non ante vel, molestie fermentum velit. Nullam auctor nibh non convallis dignissim. Nam arcu lacus, feugiat quis eros facilisis, bibendum egestas magna. Maecenas vel ipsum vitae ipsum ultricies vulputate sed ac sapien. Nunc vulputate leo varius, luctus felis eu, lacinia ex. Nulla ultrices ultrices sapien sollicitudin bibendum. Nulla ac elit magna. Fusce id suscipit libero, sed convallis odio. Donec fringilla sodales tempor.<br></br>
-
+                    ara modern k
 
                 </Typography>
 
