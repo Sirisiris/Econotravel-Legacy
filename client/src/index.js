b48c26ab2223461376583106686bc6e9e9debd18
LAPBCN-013 <lauracalderininblue5@gmail.com>
2022-07-05 15:56:32 +0200

    rutas auth

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux'
+import { legacy_createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
+const store = legacy_createStore(reducers, compose(applyMiddleware(thunk)));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
