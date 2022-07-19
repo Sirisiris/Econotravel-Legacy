dd4a33f29dcf5135d4c7ddffff91a4f4c76eb275
eperez045 <eperez045@gmail.com>
2022-07-19 15:23:44 +0200

    insert experience ok

--- server/src/controllers/experiences/insertController.ts
+++ server/src/controllers/experiences/insertController.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from "express";
 import experienceDAO from "../../model/experiences/dao";
+import Experience from '../../model/experiences/model';
 
-const experienceController = async (req: Request, res: Response) => {
+
+const insertController = {
+  insertExperience: async (req: Request, res: Response) => {
   try {
-    
-    const result = await experienceDAO.getExperiences();
+    const { title, ...Experience } = req.body as Experience;
+    console.log(req.body)
+    const result = await experienceDAO.insertExperience({ title, ...Experience  });
     result
-      ? res.json(result)
+      ? res.status(201).json(result)
       : res.status(500).send("Failed to create a new user.");
   } catch (error: any) {
     res.status(400).send(error.message);
   }
+}
 };
-export default experienceController;
+export default insertController;
--- server/src/route/experienceRoute.ts
+++ server/src/route/experienceRoute.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import experienceController from "../controllers/experiences/experienceController";
 import updateController from "../controllers/experiences/updateController";
-import insertController from "../controllers/experiences/insertController"
+import insertController from "../controllers/experiences/insertController";
 import deleteController from "../controllers/experiences/deleteController"
 
 const router = Router();
@@ -9,7 +9,7 @@ const router = Router();
 router.get('/experiences',experienceController);
 router.get('/experiences/:id',experienceController);
 router.put('/experiences/:id', updateController);
-router.post('/experiences', insertController);
+router.post('/experiences', insertController.insertExperience);
 router.delete('/experiences/:id', deleteController);
 
 export default router;
